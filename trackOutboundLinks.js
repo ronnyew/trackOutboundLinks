fd43e58f85d845c44374e659758affb6b1f16f1e
ronnyew <ronnyendreywalder@gmail.com>
2017-06-30 08:44:28 +1000

    Update comments

--- trackOutboundLinks.js
+++ trackOutboundLinks.js
@@ -1,5 +1,6 @@
 /**
-* https://support.google.com/analytics/answer/1136920?hl=en
+* Based on https://support.google.com/analytics/answer/1136920?hl=en
+*
 * Function that tracks a click on an outbound link in Analytics.
 * This function takes a valid URL string as an argument, and uses that URL string
 * as the event label. Setting the transport method to 'beacon' lets the hit be sent
